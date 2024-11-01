ef1d2f79206ff08ba820b10f9948bab093ddc2b2
ringoldsdev <ringolds@lescinskis.com>
2024-11-01 23:20:24 +0200

    chore: clean up repo

--- go.mod
+++
@@ -1,8 +0,0 @@
-module efemel
-
-go 1.20
-
-require (
-	github.com/yuin/gopher-lua v1.1.1 // indirect
-	gopkg.in/yaml.v2 v2.4.0 // indirect
-)
--- services/fileprocessors/types.go
+++
@@ -1,9 +0,0 @@
-package fileprocessors
-
-type FileProcessor interface {
-	ReadFile(filePath string) ([]byte, error)
-	WriteFile(filePath string, data []byte) error
-	FindFiles(globs []string) ([]string, error)
-	GetPathToFile(filename string) string
-	Exists(filePath string) bool
-}
